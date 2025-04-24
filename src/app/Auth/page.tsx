5851454efbca028573431576fab762b3e5abdcbd
Mikhan-TMP <mikhanbalbastro@outlook.com>
2025-04-24 13:06:43 +0800

    Improved Routing and Authguards

--- src/app/Auth/page.tsx
+++ src/app/Auth/page.tsx
@@ -3,24 +3,36 @@
 import Login from '../components/Auth/Auth';
 import AuthGuard from '../components/Auth/AuthGuard';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
 export default function Authentication() {
     const [isLoading, setIsLoading] = useState(true);
+    const router = useRouter();
 
-        useEffect(() => {
-        const timer = setTimeout(() => {
-            setIsLoading(false);
-        }, 2000);
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        const email = localStorage.getItem('userEmail');
+        const username = localStorage.getItem('userName');
 
-        return () => clearTimeout(timer);
-        }, []);
+        if (token && email && username) {
+            router.push('/Dashboard');
+        } else {
+            const timer = setTimeout(() => {
+                setIsLoading(false);
+            }, 2000);
 
-        if (isLoading) {
+            return () => clearTimeout(timer);
+        }
+    }, [router]);
+
+    if (isLoading) {
         return (
             <div className="flex items-center justify-center min-h-screen">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+                <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
             </div>
         );
-        }
-  return <Login />;
+    }
+
+    return <Login />;
 }
 
--- src/app/components/dashboard.tsx
+++ src/app/components/dashboard.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { motion } from "framer-motion";
+import { Home, Video, Users, Tv, Mic, Music, LogIn, Menu, X , SmilePlus, Sparkles  } from "lucide-react";
 import AuthGuard from './Auth/AuthGuard';
-
-import { useState } from "react";
 import ProjectEditor from "../components/projectEditor/scene-editor";
 import AvatarManagement from "../components/avatarManagement/avatar-management";
 import StudioManagement from "../components/studioManagement/studio-management";
@@ -13,161 +14,170 @@ import SceneManagerUI from "../components/updated-scene-manager";
 import AvatarGestureEmotionUI from "../components/avatarEmotion/updated-avatar-ui";
 import ProfileManagement from "../components/profile/profile";
 import HomePage from "./Home/homepage";
-import { motion } from "framer-motion";
-// import Auth from "../page";
-import { Home, Video, Users, Tv, Mic, Music, LogIn, Menu, X , SmilePlus, Sparkles  } from "lucide-react";
-// import { common } from '@mui/material/colors';
 
-const navigationItems = [
-  {
-    id: "home",
-    label: "Home",
-    icon: Home,
-    component: <HomePage />,
-  },
-  {
-    id: "avatar",
-    label: "Avatar Management",
-    icon: Users,
-    component: <AvatarManagement />,
-  },
-  {
-    id: "emotion",
-    label: "Avatar Fx",
-    icon: Sparkles  ,
-    component: <AvatarGestureEmotionUI />,
-  },
-  {
-    id: "studio",
-    label: "Studio Management",
-    icon: Tv,
-    component: <StudioManagement />,
-  },
-  {
-    id: "voice",
-    label: "Audio Management",
-    icon: Mic,
-    component: <ScriptVoiceManagement />,
-  },
-  {
-    id: "effect",
-    label: "Asset Management",
-    icon: Music,
-    component: <ContentEffectManagement />,
-  },
-  {
-    id: "scene",
-    label: "Scene Editor",
-    icon: Video,
-    component: <SceneManagerUI />,
-  },
-  {
-    id: "project",
-    label: "Project Editor",
-    icon: Video,
-    component: <ProjectEditor />,
-  },
-  {
-    id: "user",
-    label: localStorage.getItem('userName') ? localStorage.getItem('userName') : "Login/Signup",
-    icon: localStorage.getItem('userName') ? Users : LogIn,
-    component: <ProfileManagement />
-  },
-];
 
-    const Page = () => {
-    const [currentView, setCurrentView] = useState("home");
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+const Page = () => {
+  const router = useRouter();
+  const [currentView, setCurrentView] = useState("home");
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [username, setUsername] = useState("Login/Signup");
+  const [token, setToken] = useState('');
 
-    return (
-      <AuthGuard>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 2, ease: 'easeInOut' }}
-            >
-            <div className="min-h-screen bg-gray-100">
-                <style jsx global>{`body {color: black;}`}</style>
-                    {/* Navigation */}
-                    <nav className="bg-white shadow-sm p-2 sticky top-0 z-50 w-full flex justify-content-between">
-                    <div className="max-w-1xl mx-auto px-4 flex p-4 items-center h-16">
-                    {/* Logo */}
-                    <img src="http://www.meta-town.io/Metacity/assets/Flexor/assets/img/metatown.png" alt="MetaTown Logo" className="h-8 w-auto pe-10"/>
+  useEffect(() => {
+    const storedUser = localStorage.getItem('userName');
+    const storedToken = localStorage.getItem('token');
 
-                    {/* Desktop Navigation */}
-                <div className="hidden lg:flex flex-wrap gap-2">
-                    {navigationItems.map(({ id, label, icon: Icon }) => (
-                        <button
-                        key={id}
-                        onClick={() => setCurrentView(id)}
-                        className={`flex items-center justify-center text-sm min-w-[130px] max-w-[180px] h-12 border-b-2 cursor-pointer transition-all whitespace-nowrap px-3 ${
-                            currentView === id
-                            ? "border-[#9B25A7] text-[#7A1C86]"
-                            : "border-transparent text-gray-500 hover:text-[#9B25A7] hover:border-[#9B25A7]"
-                        }`}
-                        >
-                        <Icon className="mr-2 h-5 w-5 flex-shrink-0" />
-                        <span className="truncate">{label}</span>
-                        </button>
-                    ))}
-                </div>
-                    {/* Mobile Menu Button */}
-                    <button
-                    className="lg:hidden p-2 ml-auto"
-                    onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-                    >
-                    {mobileMenuOpen ? (
-                        <X className="h-6 w-6 text-gray-600" />
-                    ) : (
-                        <Menu className="h-6 w-6 text-gray-600" />
-                    )}
-                    </button>
+    if (storedUser && storedToken) {
+      setUsername(storedUser);
+      setToken(storedToken);
+    }
+  }, []);
+
+  const navigationItems = [
+    {
+      id: "home",
+      label: "Home",
+      icon: Home,
+      component: <HomePage />,
+    },
+    {
+      id: "avatar",
+      label: "Avatar Management",
+      icon: Users,
+      component: <AvatarManagement />,
+    },
+    {
+      id: "emotion",
+      label: "Avatar Fx",
+      icon: Sparkles,
+      component: <AvatarGestureEmotionUI />,
+    },
+    {
+      id: "studio",
+      label: "Studio Management",
+      icon: Tv,
+      component: <StudioManagement />,
+    },
+    {
+      id: "voice",
+      label: "Audio Management",
+      icon: Mic,
+      component: <ScriptVoiceManagement />,
+    },
+    {
+      id: "effect",
+      label: "Asset Management",
+      icon: Music,
+      component: <ContentEffectManagement />,
+    },
+    {
+      id: "scene",
+      label: "Scene Editor",
+      icon: Video,
+      component: <SceneManagerUI />,
+    },
+    {
+      id: "project",
+      label: "Project Editor",
+      icon: Video,
+      component: <ProjectEditor />,
+    },
+    {
+      id: "user",
+      label: username,
+      icon: username !== "Login/Signup" ? Users : LogIn,
+      component: <ProfileManagement />
+    },
+  ];
+
+  return (
+    <AuthGuard>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, ease: 'easeInOut' }}
+      >
+        <div className="min-h-screen bg-gray-100">
+          <style jsx global>{`body {color: black;}`}</style>
+          {/* Navigation */}
+          <nav className="bg-white shadow-sm p-2 sticky top-0 z-50 w-full flex justify-content-between">
+            <div className="max-w-1xl mx-auto px-4 flex p-4 items-center h-16">
+              {/* Logo */}
+              <img src="http://www.meta-town.io/Metacity/assets/Flexor/assets/img/metatown.png" alt="MetaTown Logo" className="h-8 w-auto pe-10 cursor-pointer" onClick={() => router.push('/')} />
+
+              {/* Desktop Navigation */}
+              <div className="hidden lg:flex flex-wrap gap-2">
+                {navigationItems.map(({ id, label, icon: Icon }) => (
+                  <button
+                    key={id}
+                    onClick={() => setCurrentView(id)}
+                    className={`flex items-center justify-center text-sm min-w-[130px] max-w-[180px] h-12 border-b-2 cursor-pointer transition-all whitespace-nowrap px-3 ${
+                      currentView === id
+                        ? "border-[#9B25A7] text-[#7A1C86]"
+                        : "border-transparent text-gray-500 hover:text-[#9B25A7] hover:border-[#9B25A7]"
+                    }`}
+                  >
+                    <Icon className="mr-2 h-5 w-5 flex-shrink-0" />
+                    <span className="truncate">{label}</span>
+                  </button>
+                ))}
+              </div>
+              {/* Mobile Menu Button */}
+              <button
+                className="lg:hidden p-2 ml-auto"
+                onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+              >
+                {mobileMenuOpen ? (
+                  <X className="h-6 w-6 text-gray-600" />
+                ) : (
+                  <Menu className="h-6 w-6 text-gray-600" />
+                )}
+              </button>
             </div>
 
-          {/* Mobile Menu */}
-          {mobileMenuOpen && (
+            {/* Mobile Menu */}
+            {mobileMenuOpen && (
+              <div
+                className="fixed inset-0 bg-white bg-opacity-50 backdrop-blur backdrop-filter z-40"
+                onClick={() => setMobileMenuOpen(false)}
+              ></div>
+            )}
             <div
-              className="fixed inset-0 bg-white bg-opacity-50 backdrop-blur backdrop-filter z-40"
-              onClick={() => setMobileMenuOpen(false)}
-            ></div>
-          )}
-          <div
-            className={`lg:hidden fixed top-0 right-0 w-64 h-full bg-white shadow-lg p-4 transition-transform transform z-50 ${
-              mobileMenuOpen ? "translate-x-0" : "translate-x-full"
-            }`}
-          >
-            <button
-              className="absolute top-4 right-4"
-              onClick={() => setMobileMenuOpen(false)}
+              className={`lg:hidden fixed top-0 right-0 w-64 h-full bg-white shadow-lg p-4 transition-transform transform z-50 ${
+                mobileMenuOpen ? "translate-x-0" : "translate-x-full"
+              }`}
             >
-              <X className="h-6 w-6 text-gray-600" />
-            </button>
-            <div className="flex flex-col mt-10 space-y-4">
-              {navigationItems.map(({ id, label, icon: Icon }) => (
-                <button
-                  key={id}
-                  onClick={() => {
-                    setCurrentView(id);
-                    setMobileMenuOpen(false);
-                  }}
-                  className="flex items-center w-full px-4 py-2 text-gray-600 hover:text-gray-800 hover:bg-gray-200 rounded-lg"
-                >
-                  <Icon className="mr-3 h-5 w-5" />
-                  {label}
-                </button>
-              ))}
+              <button
+                className="absolute top-4 right-4"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <X className="h-6 w-6 text-gray-600" />
+              </button>
+              <div className="flex flex-col mt-10 space-y-4">
+                {navigationItems.map(({ id, label, icon: Icon }) => (
+                  <button
+                    key={id}
+                    onClick={() => {
+                      setCurrentView(id);
+                      setMobileMenuOpen(false);
+                    }}
+                    className="flex items-center w-full px-4 py-2 text-gray-600 hover:text-gray-800 hover:bg-gray-200 rounded-lg"
+                  >
+                    <Icon className="mr-3 h-5 w-5" />
+                    {label}
+                  </button>
+                ))}
+              </div>
             </div>
-          </div>
-        </nav>
+          </nav>
 
-        {/* Content */}
-        <main className="max-w-8xl mx-auto pt-4 px-2 pb-4 rounded-2xl w-full min-h-[120vh]">
-          {navigationItems.find((item) => item.id === currentView)?.component}
-        </main>
-            </div>
-          </motion.div>
-      </AuthGuard>
-      
+          {/* Content */}
+          <main className="max-w-8xl mx-auto pt-4 px-2 pb-4 rounded-2xl w-full min-h-[120vh]">
+            {navigationItems.find((item) => item.id === currentView)?.component}
+          </main>
+        </div>
+      </motion.div>
+    </AuthGuard>
   );
 };
 
